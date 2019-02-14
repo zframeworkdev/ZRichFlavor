3caf6c248a7470659a57965ff5f2acce453bbdd3
Zaldy <zframeworkdev@gmail.com>
2019-02-14 09:49:32 +0800

    Initial commit

---
+++ ZRichFlavor/Models/SPerson.swift
@@ -0,0 +1,13 @@
+//
+//  SPerson.swift
+//  ZRichFlavor
+//
+//  Created by Zaldy on 14/02/2019.
+//  Copyright © 2019 InternetBrands. All rights reserved.
+//
+
+import Foundation
+
+struct SPerson {
+    var name: String?
+}
