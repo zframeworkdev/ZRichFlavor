e9ba18c9889f3d50cd8d8daaec59dd41ed36f1d1
Zaldy <zframeworkdev@gmail.com>
2019-02-14 09:19:46 +0800

    Initial Commit

---
+++ ZRichFlavor/ZRichFlavor.h
@@ -0,0 +1,19 @@
+//
+//  ZRichFlavor.h
+//  ZRichFlavor
+//
+//  Created by Zaldy on 14/02/2019.
+//  Copyright © 2019 InternetBrands. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZRichFlavor.
+FOUNDATION_EXPORT double ZRichFlavorVersionNumber;
+
+//! Project version string for ZRichFlavor.
+FOUNDATION_EXPORT const unsigned char ZRichFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZRichFlavor/PublicHeader.h>
+
+
